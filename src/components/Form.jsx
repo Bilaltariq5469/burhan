1958ba473f24f3e1ca6234c0ffefbdfcd26613e4
Bilaltariq5469 <35967967+Bilaltariq5469@users.noreply.github.com>
2020-09-18 03:15:25 +0500

    * loaded added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,73 +1,88 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import useForm from '../hooks/useForm';
 import validate from '../utils/validateLogin';
 import { ToastContainer } from 'react-toastify';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Spinner from './Spinner';
+import { css } from "@emotion/core";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const Form = () => {
-  const { handleChange, handleSubmit, values, errors } = useForm(
-    submit,
-    validate
+  const { handleChange, handleSubmit, values, errors, Loading } = useForm(
+      submit,
+      validate
   );
+  const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: grey;
+`;
   function submit() {
     //need to run the <spinner /> here before the toast
     toast.success('Submitted successfully');
   }
 
   return (
-    <React.Fragment>
-      <ToastContainer />
+      <React.Fragment>
+        <ToastContainer />
 
-      <div className="container">
-        <div className="row">
-          <div className="col-6 mx-auto">
-            <div className="card">
-              <div className="card-header">
-                <p className="h3 text-center">Safepay</p>
-              </div>
-              <div className="card-body">
-                <form className="form" onSubmit={handleSubmit} noValidate>
-                  <div className="form-group mx-sm-3 mb-2">
-                    <label>Email</label>
-                    <input
-                      name="email"
-                      type="email"
-                      className="form-control border-top-0 border-left-0 border-right-0 pl-0"
-                      value={values.email}
-                      onChange={handleChange}
-                      placeholder="john.doe@example.com"
-                    ></input>
+        <div className="container">
+          <div className="row">
+            <div className="col-6 mx-auto">
+              <div className="card">
+                <div className="card-header">
+                  <p className="h3 text-center">Safepay</p>
+                </div>
+                <div className="card-body">
+                  <form className="form" onSubmit={handleSubmit} noValidate>
+                    <div className="form-group mx-sm-3 mb-2">
+                      <label>Email</label>
+                      <input
+                          name="email"
+                          type="email"
+                          className="form-control border-top-0 border-left-0 border-right-0 pl-0"
+                          value={values.email}
+                          onChange={handleChange}
+                          placeholder="john.doe@example.com"
+                      ></input>
 
-                    {errors.email && <p className="error">{errors.email}</p>}
-                  </div>
-                  <div className="form-group mx-sm-3 mb-2">
-                    <label htmlFor="password">Password</label>
-                    <input
-                      name="password"
-                      type="password"
-                      className="form-control border-top-0 border-left-0 border-right-0 pl-0"
-                      placeholder=".........."
-                      value={values.password}
-                      onChange={handleChange}
-                    ></input>
-                    {errors.password && (
-                      <p className="error">{errors.password}</p>
-                    )}
-                  </div>
-                  <div className="col-12 text-center">
-                    <button type="submit" className="btn btn-dark mt-3 spinner">
-                      LOG IN
-                    </button>
-                  </div>
-                </form>
+                      {errors.email && <p className="error">{errors.email}</p>}
+                    </div>
+                    <div className="sweet-loading">
+                      <ClipLoader
+                          css={override}
+                          size={60}
+                          color={"#FFFFFF"}
+                          loading={Loading.value}
+                      />
+                    </div>
+                    <div className="form-group mx-sm-3 mb-2">
+                      <label htmlFor="password">Password</label>
+                      <input
+                          name="password"
+                          type="password"
+                          className="form-control border-top-0 border-left-0 border-right-0 pl-0"
+                          placeholder=".........."
+                          value={values.password}
+                          onChange={handleChange}
+                      ></input>
+                      {errors.password && (
+                          <p className="error">{errors.password}</p>
+                      )}
+                    </div>
+                    <div className="col-12 text-center">
+                      <button type="submit" className="btn btn-dark mt-3 spinner">
+                        LOG IN
+                      </button>
+                    </div>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </React.Fragment>
+      </React.Fragment>
   );
 };
 
--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -5,6 +5,7 @@ const useForm = (callback, validate) => {
   const [values, setValues] = useState({ email: '', password: '' });
   const [errors, setErrors] = useState({});
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [Loading, setLoading] = useState({value:false});
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -18,9 +19,17 @@ const useForm = (callback, validate) => {
 
     setErrors(validate(values));
     setIsSubmitting(true);
+    setLoading(true);
+    setTimeout(function () {
+      console.log("Called setLoading");
+      setLoading({value:false});
+    }, 1500);
   };
   useEffect(() => {
     if (Object.keys(errors).length === 0 && isSubmitting) {
+      // setLoading(false);
+      // setIsSubmitting(false);
+      // console.log("Called");
       //call the server
       callback();
     } else toast.error('Invalid email or password');
@@ -31,6 +40,7 @@ const useForm = (callback, validate) => {
     handleSubmit,
     values,
     errors,
+    Loading
   };
 };
 
